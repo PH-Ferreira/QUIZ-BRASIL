501a4234972dc9ca9e5a0b45d96f89a76093ae21
PH-Ferreira <104328978+PH-Ferreira@users.noreply.github.com>
2022-06-10 21:40:02 -0300

    Create questões.js

---
+++ questões.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Quem descobriu o Brasil?",
+    answer: "Pedro Alvarez Cabral",
+    options: [
+      "Dom Pedro I",
+      "Dom Pedro II",
+      "Pedro Alvarez Cabral",
+      "Lucas Ferreira"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Em que data foi descoberto?",
+    answer: "22 de abril de 1500",
+    options: [
+      "7 de setembro de 1500",
+      "9 de julho de 1970",
+      "22 de abril de 1500",
+      "20 de janeiro de 1730"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual foi o primeiro nome do Brasil?",
+    answer: "Ilha de Vera Cruz",
+    options: [
+      "Brazil",
+      "Ilha de Vera Cruz",
+      "Terra de Vera Cruz",
+      "Brasil"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quem foi o primeiro presidente da Replùblica?",
+    answer: "Marechal Deodoro da Fonseca",
+    options: [
+      "Neymar",
+      "Dom Pedro I",
+      "Dom Pedro II",
+      "Marechal Deodoro da Fonseca"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quantos estados formam o Brasil?",
+    answer: "28 estados e 1 Distrito Federal",
+    options: [
+      "27 estados e 1 Distrito Federal",
+      "26 estados e 1 Distrito Federal",
+      "28 estados e 1 Distrito Federal",
+      "25 estados e 1 Distrito Federal"
+    ]
+  },
+];
